e668c03adc61b737b02f9564807b78855922a3c3
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2019-09-04 23:44:01 +0530

    Event Bubbling and Capturing

---
+++ event_bubbling_capturing/main.js
@@ -0,0 +1,28 @@
+// addEventListener(event, function, useCapture);
+
+// true - The event handler is executed in the capturing phase
+// false- Default. The event handler is executed in the bubbling phase
+
+const evtBubblingDivs = document.querySelectorAll('.evt-bubbling div');
+const evtCapturingDivs = document.querySelectorAll('.evt-capturing div');
+
+function handleClick(e) {
+    console.log(this.className);
+}
+
+function handleBodyClick(e) {
+    // alert('Welcome');
+    console.log('Welcome');
+}
+
+console.log(evtBubblingDivs);
+evtBubblingDivs.forEach( (div) => {
+    div.addEventListener('click', handleClick, false);
+});
+
+console.log(evtCapturingDivs);
+evtCapturingDivs.forEach( (div) => {
+    div.addEventListener('click', handleClick, true);
+});
+
+document.body.addEventListener('click', handleBodyClick, true);
