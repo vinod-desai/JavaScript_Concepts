924cd8b2685a11a4ad12835df028ba0504a91c80
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2019-11-02 23:40:01 +0530

    Add files via upload

--- polyfills_JS/app.js
+++ polyfills_JS/app.js
@@ -1,5 +1,3 @@
-// Polyfill for array map method (Higher Order Array functions)
-
 let myArray = [1,2,3,4,5];
 
 const double = (item) => item * 2;
@@ -14,6 +12,8 @@ const users = [
 ];
 
 const userName = (user) => user.name;
+
+// Polyfill for array map method (Higher Order Array functions)
 
 // let newArray = myArray.map(double);
 
@@ -35,11 +35,35 @@ Array.prototype.myMap = function (fn) {
 // console.log(myArray);
 // console.log(newArray);
 
-let newArray = users.myMap(userName);
-console.log(users);
-console.log(newArray);
+// let newArray = users.myMap(userName);
+// console.log(users);
+// console.log(newArray);
 
 // Polyfill for array filter method
+
+
+// let newArray = myArray.filter((element) => element % 2 === 0);
+
+Array.prototype.myFilter = function(fn){
+    const filteredArray = [];
+    const context = this;
+
+    for(let i=0; i<context.length; i++){
+        if(fn(context[i])){
+            filteredArray.push(context[i]);
+        }
+        
+    }
+    return filteredArray;
+}
+
+const getEvenNumbers = (num) =>  num % 2 === 0 ;
+const getOddNumbers = (num) => num % 2 !== 0;
+
+let newArray = myArray.myFilter(getOddNumbers);
+
+console.log(myArray);
+console.log(newArray);
 
 
 // // Polyfill for array reduce method
